67f69b8ad591876383d77087bf279fb85b4fb08b
kendrewchris <kendrwchristanto@gmail.com>
2022-09-22 00:41:36 -0700

    Fixed modal window test display. Only displays in the instance that a movie trailer object is not found

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-/* 
-🌟 APP: Make Netflix
-
-Here we have the Netflix app but it's up to you to make it work by pulling all the movies using an API!
-
-Create a fetchMovies() function that will make a dynamic API call to what you need 👇
-========================================
-
-- fetchMovies()
-
-** fetchMovies takes in an URL, a div id or class from the HTML, and a path (poster or backdrop)
-
-
-
-These are the 3 main functions and their URL'S you must create  👇
-========================================
-
-- getOriginals()
-  * URL : 'https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
-
-- getTrendingNow()
-  * URL : 'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045'
-
-- getTopRated()
-  * URL : 'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'
-
-
-** These functions will provide the URL you need to fetch() movies of that genere **
-
-These are all the DIV ID's you're gonna need access to 👇
-========================================================
-#1 CLASS 👉 'original__movies' = Div that holds Netflix Originals
-#2 ID 👉 'trending' = Div that holds trending Movies
-#3 ID 👉 'top_rated' = Div that holds top rated Movies
-*/
 
 //div variables
 let originalsDiv = document.getElementById('originalMovies');
@@ -61,9 +26,7 @@ function fetchMovies(url, dom_element, path) {
     .then(data=>{
       // iterates through entire array of movie objects returned from API call
       // creates new img elements and appends to corresponding div assigned to that movie type
-
       for(i=0;i<data.results.length;i++){
-        console.log(data.results.length);
         let tempElem = document.createElement("img");
         tempElem.setAttribute("src", String(rootUrl + data.results[i][path]));
         tempElem.setAttribute("data-id", String(data.results[i].id));
@@ -82,23 +45,28 @@ function fetchMovies(url, dom_element, path) {
 function setTrailer(e){
   let ytRoot = 'https://www.youtube.com/embed/';
   let id = e.target.getAttribute('data-id');
+  let trailerFoundDiv = document.getElementById('notFound');
   getMovieTrailer(id).then(data =>{
-    console.log(data['results']);
     if(data['results'].length > 0){
-      modalWindow.classList.add('d-none');
-      modalWindow.classList.remove('d-none');
-
+      
       let key = String(data['results'][0]['key']);
-      console.log(data['results']);
       modalWindow.setAttribute('src', ytRoot+key);
-      $('#trailerModal').modal('show');
+      trailerFoundDiv.style.display = 'none';
     }
-    else{
-      modalWindow.classList.add('d-none');
-      modalWindow.classList.remove('d-none');
+    else if (data['results'].length == 0){
+      trailerFoundDiv.style.display = '';
+      modalWindow.setAttribute('src', '');
     }
     $('#trailerModal').modal('show');
   })
+  .catch( err =>{
+    if(err){
+      trailerFoundDiv.style.display = '';
+      modalWindow.setAttribute('src', '');
+    }
+    $('#trailerModal').modal('show');
+  }
+  )
 }
 
 // ** Function that fetches Netflix Originals **
@@ -131,24 +99,6 @@ async function getMovieTrailer(id) {
   });
 
 }
-
-// ** Function that adds movie data to the DOM
-// const setTrailer = trailers => {
-//   // Set up iframe variable to hold id of the movieTrailer Element
-//   const iframe
-//   // Set up variable to select .movieNotFound element
-//   const movieNotFound
-
-//   // If there is a trailer add the src for it
-//   if (trailers.length > 0) {
-//     // add d-none class to movieNotFound and remove it from iframe
-
-//     // add youtube link with trailers key to iframe.src
-//   } else {
-//     // Else remove d-none class to movieNotfound and ADD it to iframe
-
-//   }
-// }
 
 
 
